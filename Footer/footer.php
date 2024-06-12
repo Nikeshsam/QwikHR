679eaa135261a52df98faf20df8459d4b3a4f266
Nikeshsam <nikesh.designer@gmail.com>
2024-06-12 12:45:56 +0530

    Change the file to PHP

---
+++ Footer/footer.php
@@ -0,0 +1,39 @@
+
+<script type="text/javascript" src="../assets/js/jquery-3.4.1.min.js"></script>
+
+    <!-------------------------- BOOTSTRAP -------------------------->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.0.4/js/bootstrap5-toggle.jquery.min.js"></script>
+    <!-------------------------- BOOTSTRAP -------------------------->
+
+    <!-------------------------- SIMPLE-SCROLL-BAR -------------------------->
+    <script type="text/javascript" src="../assets/js/simplebar.js"></script>
+    <!-------------------------- SIMPLE-SCROLL-BAR -------------------------->
+    
+    <!-------------------------- BOOTSTRP DATE PICKER -------------------------->    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <!-------------------------- BOOTSTRP DATE PICKER -------------------------->
+
+    <!-------------------------- CUSTOM TABLE -------------------------->
+    <script type="text/javascript" src="../assets/js/table.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.4/dist/bootstrap-table.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.4/dist/extensions/fixed-columns/bootstrap-table-fixed-columns.min.js"></script>
+    <!-------------------------- CUSTOM TABLE -------------------------->
+
+    <!-------------------------- ADAPTING MENU BUTTON -------------------------->
+    <script type="text/javascript" src="../assets/js/vertical-menu-adapting-with-button.js"></script>
+    <script type="text/javascript" src="../assets/js/horizontal-menu-adapting-with-button.js"></script>
+    <!-------------------------- ADAPTING MENU BUTTON -------------------------->
+
+    <!-------------------------- ORGANIZATION -------------------------->
+    <script type="text/javascript" src="../assets/js/organization.js"></script>
+    <!-------------------------- ORGANIZATION -------------------------->
+
+    <script type="text/javascript" src="../assets/js/slick.min.js"></script>
+    <script type="text/javascript" src="../assets/js/custom.js"></script>  
+
+    <!-- <script type="text/javascript" src="../assets/js/jquery-ui.js"></script>   -->
+</body>
+</html>
